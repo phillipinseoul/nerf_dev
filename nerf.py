3a258630cf0a913217a89689013a7d5de5ed73f1
Yuseung Lee <phillip9807@gmail.com>
2022-08-30 20:50:13 +0900

    define HarmonicEmbedding

--- nerf.py
+++ nerf.py
@@ -131,11 +131,37 @@ to produce a full-sized render of a scene.
 '''
 class HarmonicEmbedding(torch.nn.Module):
     def __init__(self, n_harmonic_functions=60, omega0=0.1):
-       return
+       '''
+       Takes input tensor `x`: [minibatch, ..., dim], and converts each feature in `x` 
+       into a series of harmonic features `embedding` as follows:
+            embedding[..., i*dim : (i+1)*dim] = [
+                sin(x[..., i]),
+                sin(2 * x[..., i]),
+                sin(4 * x[..., i]),
+                ...
+                sin(2 ** (self.n_harmonic_functions - 1) * x[..., i]),
+                cos(x[..., i]),
+                cos(2 * x[..., i]),
+                cos(4 * x[..., i]),
+                ...
+                cos(2 ** (self.n_harmonic_functions - 1) * x[..., i])
+            ]
+       '''
+       super().__init__()
+       self.register_buffer(
+        'frequencies',
+        omega0 * (2.0 ** torch.arange(n_harmonic_functions)),
+       )
 
     def forward(self, x):
-        return
-
+        '''
+        input:
+            - x: [..., dim]
+        output:
+            - embedding: a `harmonic embedding of x`: [..., n_harmonic_functions * dim * 2]
+        '''
+        embed = (x[..., None] * self.frequencies).view(*x.shape[:-1], -1)
+        return torch.cat((embed.sin(), embed.cos()), dim=-1)
     
 class NeuralRadianceField(torch.nn.Module):
     def __init__(self, n_harmonic_functions=60, n_hidden_neurons=256):
